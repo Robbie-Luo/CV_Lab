80a54dd6bb7b1a792d8832d12a44fb08137e9ff6
yz2691 <33187793+yz2691@users.noreply.github.com>
2019-02-26 12:31:08 +0100

    Update denoise.m

--- Lab2/Part-1: Image Enhancement/denoise.m
+++ Lab2/Part-1: Image Enhancement/denoise.m
@@ -1,11 +1,18 @@
-function [ imOut ] = denoise( image, kernel_type, varargin)
+function [ imOut ] = denoise(image, kernel_type, varargin)
+I = imread(image);
 
 switch kernel_type
     case 'box'
-        fprintf('Not implemented\n')
+        filterSize = varargin{1};
+        imOut = imboxfilt(I,filterSize);
     case 'median'
-        fprintf('Not implemented\n')
+        filterSize = varargin{1};
+        imOut = medfilt2(I,[filterSize,filterSize]);
     case 'gaussian'
-        fprintf('Not implemented\n')
+        sigma = varargin{1};
+        filterSize = varargin{2};
+        G = gauss2D(sigma,filterSize);
+        imOut = conv2(I,G,'same');
 end
+
 end
