1d4384c689d2bac47ea815ef06cefc72831d810e
Liang Huang <huangliang0826@icloud.com>
2019-02-26 10:28:58 +0100

    Correct some errors in creatGabor.m

--- Lab2/Part-2: Gabor Segmentation/createGabor.m
+++ Lab2/Part-2: Gabor Segmentation/createGabor.m
@@ -58,8 +58,8 @@ sinCarrier = createSin(rot_x, lambda, psi);
 
 % Modulate (multiply) Gaussian envelope with the carriers to compute 
 % the real and imaginary components of the omplex Gabor filter. 
-myGabor_real =  gaussianEnv*cosCarrier;    % \\TODO: modulate gaussianEnv with cosCarrier
-myGabor_imaginary = gaussianEnv*sinCarrier;% \\TODO: modulate gaussianEnv with sinCarrier
+myGabor_real =  gaussianEnv.*cosCarrier;    % \\TODO: modulate gaussianEnv with cosCarrier
+myGabor_imaginary = gaussianEnv.*sinCarrier;% \\TODO: modulate gaussianEnv with sinCarrier
 
 % Pack myGabor_real and myGabor_imaginary into myGabor.
 myGabor(:,:,1) = myGabor_real;
@@ -84,7 +84,7 @@ end
 function cosCarrier = createCos(rot_x, lambda, psi)
 % ----------------------------------------------------------
 % Returns the 2D cosine carrier. 
-cosCarrier = cos(2*pi*rot_x/lambda + psi);
+cosCarrier = cos((2*pi*rot_x)/lambda + psi);
 % \\TODO: Implement the cosine given rot_x, ;lambda and psi.
 
 % Reshape the vector representation to matrix.
@@ -95,7 +95,7 @@ end
 function sinCarrier = createSin(rot_x, lambda, psi)
 % ----------------------------------------------------------
 % Returns the 2D sine carrier. 
-sinCarrier = sin(2*pi*rot_x/lambda + psi);
+sinCarrier = sin((2*pi*rot_x)/lambda + psi);
 % \\TODO: Implement the sine given rot_x, lambda and psi.
 
 % Reshape the vector representation to matrix.
@@ -106,7 +106,7 @@ end
 function gaussEnv = createGauss(rot_x, rot_y, gamma, sigma)
 % ----------------------------------------------------------
 % Returns the 2D Gaussian Envelope. 
-gaussEnv = exp(-(rot_x.^2 + (gamma.^2)*(rot_y.^2))/2*sigma);% \\TODO: Implement the Gaussian envelope.
+gaussEnv = exp(-(rot_x.^2 + (gamma.^2)*(rot_y.^2))/(2*sigma*sigma));% \\TODO: Implement the Gaussian envelope.
 
 % Reshape the vector representation to matrix.
 gaussEnv = reshape(gaussEnv, sqrt(length(gaussEnv)), []);
