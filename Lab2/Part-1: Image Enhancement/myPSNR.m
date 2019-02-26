d534b447f277f542e8230f92780a2099bf7cf621
max <doermax@outlook.com>
2019-02-26 11:42:20 +0100

    added compute_gradient and PSNR scripts

--- Lab2/Part-1: Image Enhancement/myPSNR.m
+++ Lab2/Part-1: Image Enhancement/myPSNR.m
@@ -1,5 +1,14 @@
-function [ PSNR ] = myPSNR( orig_image, approx_image )
-fprintf('Not implemented\n')
+function [PSNR] = myfoo( orig_image, approx_image )
+
+m = size(orig_image, 1);
+n = size(approx_image, 2);
+
+mse = 1/ (m*n) * sum(sum((orig_image - approx_image).^2)) ; 
+
+I_max = double(max( max(orig_image) )) ; 
+
+score_1 = 10 * log10( I_max ^2 / mse) ;
+
+PSNR = score_1 ; 
 
 end
-
