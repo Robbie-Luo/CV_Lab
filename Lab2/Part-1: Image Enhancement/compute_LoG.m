1529d43a575ed9816c91aa4d9069f78ff4642b04
yz2691 <33187793+yz2691@users.noreply.github.com>
2019-02-27 20:31:14 +0100

    Update compute_LoG.m

--- Lab2/Part-1: Image Enhancement/compute_LoG.m
+++ Lab2/Part-1: Image Enhancement/compute_LoG.m
@@ -5,20 +5,18 @@ switch LOG_type
         %method 1
         k_gauss = fspecial('gaussian', 5, 0.5);
         k_laplace = fspecial('laplacian');
-        smoothed = imfilter(image, k_gauss, 'same');
-        imOut = imfilter(smoothed, k_laplace, 'same');
+        smoothed = imfilter(image, k_gauss, 'same', 'conv');
+        imOut = imfilter(smoothed, k_laplace, 'same', 'conv');
     case 2
         %method 2
-        k = fspecial('log', 5, 0.5);
-        imOut = imfilter(image, k);
+        k_laplace = fspecial('log', 5, 0.5);
+        imOut = imfilter(image, k_laplace, 'same', 'conv');
     case 3
         %method 3
         k_gauss1 = fspecial('gaussian', 5, 0.5);
-        k_gauss2 = fspecial('gaussian', 5, 4);
-        
-        gaussian1 = imfilter(image, k_gauss1);
-        gaussian2 = imfilter(image, k_gauss2);
-        
+        k_gauss2 = fspecial('gaussian', 5, 30);
+        gaussian1 = imfilter(image, k_gauss1, 'same', 'conv');
+        gaussian2 = imfilter(image, k_gauss2, 'same', 'conv');
         imOut = gaussian1 - gaussian2;
 end
 end
