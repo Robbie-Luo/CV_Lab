2c6d30a08f7dce6432bf8d1b62c945842e2e9050
yz2691 <33187793+yz2691@users.noreply.github.com>
2019-02-27 19:52:34 +0100

    Update myPSNR.m

--- Lab2/Part-1: Image Enhancement/myPSNR.m
+++ Lab2/Part-1: Image Enhancement/myPSNR.m
@@ -1,14 +1,20 @@
-function [PSNR] = myfoo( orig_image, approx_image )
+function [PSNR] = myPSNR( orig_image, approx_image )
 
-m = size(orig_image, 1);
-n = size(approx_image, 2);
+orig_image_double = im2double(orig_image);
+approx_image_double = im2double(approx_image);
 
-mse = 1/ (m*n) * sum(sum((orig_image - approx_image).^2)) ; 
+[m,n] = size(orig_image_double);
 
-I_max = double(max( max(orig_image) )) ; 
+%MSE = immse(orig_image_double, approx_image_double);
+MSE = 0;
+for i = 1:m
+    for j = 1:n
+        MSE = MSE + (orig_image_double(i,j) - approx_image_double(i,j)).^2;
+    end
+end
 
-score_1 = 10 * log10( I_max ^2 / mse) ;
-
-PSNR = score_1 ; 
+MSE = MSE / (m*n);
+RMSE = sqrt(MSE);
+PSNR = 20 * log10(max(max(orig_image_double)) / RMSE);
 
 end
