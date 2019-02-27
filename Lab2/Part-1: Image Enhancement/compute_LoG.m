4be227a76a02701a14a514fd40a8a15b149f1227
yz2691 <33187793+yz2691@users.noreply.github.com>
2019-02-27 17:23:07 +0100

    Update compute_LoG.m

--- Lab2/Part-1: Image Enhancement/compute_LoG.m
+++ Lab2/Part-1: Image Enhancement/compute_LoG.m
@@ -3,16 +3,23 @@ function imOut = compute_LoG(image, LOG_type)
 switch LOG_type
     case 1
         %method 1
-        fprintf('Not implemented\n')
-
+        k_gauss = fspecial('gaussian', 5, 0.5);
+        k_laplace = fspecial('laplacian');
+        smoothed = imfilter(image, k_gauss, 'same');
+        imOut = imfilter(smoothed, k_laplace, 'same');
     case 2
         %method 2
-        fprintf('Not implemented\n')
-
+        k = fspecial('log', 5, 0.5);
+        imOut = imfilter(image, k);
     case 3
         %method 3
-        fprintf('Not implemented\n')
-
+        k_gauss1 = fspecial('gaussian', 5, 0.5);
+        k_gauss2 = fspecial('gaussian', 5, 4);
+        
+        gaussian1 = imfilter(image, k_gauss1);
+        gaussian2 = imfilter(image, k_gauss2);
+        
+        imOut = gaussian1 - gaussian2;
 end
 end
 
