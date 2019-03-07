483f22c2b3dda7d0a0ae14c0b221091cb46ba179
Robbie <luobutang@gmail.com>
2019-03-07 15:29:28 +0100

    implemented keypoint matching

--- Lab4/keypoint_matching.m
+++ Lab4/keypoint_matching.m
@@ -4,7 +4,7 @@ function [fa_matches,fb_matches]=keypoint_matching(Ia,Ib)
     Ib=single(Ib);
     [fa, da] = vl_sift(Ia) ;
     [fb, db] = vl_sift(Ib) ;
-    [matches, scores] = vl_ubcmatch(da, db) ;
+    [matches, ~] = vl_ubcmatch(da, db) ;
     fa_matches = fa(:,matches(1,:));
     fb_matches = fb(:,matches(2,:));
 end
--- Lab4/plot_matches.m
+++ Lab4/plot_matches.m
@@ -1,26 +1,22 @@
 clc;clear all;close all;
 Ia=imread("boat1.pgm");
-Ib=imread("boat2.pgm");
-[fa_matches,fb_matches]=keypoint_matching(Ia,Ib);
-subplot(1,2,1)
-imshow(Ia);
-hold on;
-h1 = vl_plotframe(fa_matches(:,1:100)) ;
-set(h1,'color','y','linewidth',3) ;
+Ib=imread("boat2.pgm");imshow([Ia Ib]) ;
+[fa,fb]=keypoint_matching(Ia,Ib);
 
-subplot(1,2,2)
-imshow(Ib);
-hold on;
-h1 = vl_plotframe(fb_matches(:,1:100)) ;
-set(h1,'color','y','linewidth',3) ;
+perm=randperm(size(fa,2));
+sel=perm(1:10)
 
-function []=visualize_sift(f,d)
-    perm = randperm(size(f,2)) ;
-    sel = perm(1:50) ;
-    h1 = vl_plotframe(f(:,sel)) ;
-    h2 = vl_plotframe(f(:,sel)) ;
-    set(h1,'color','k','linewidth',3) ;
-    set(h2,'color','y','linewidth',2) ;
-    h3 = vl_plotsiftdescriptor(d(:,sel),f(:,sel)) ;
-    set(h3,'color','g') ;
-end
+hold on;
+h1 = vl_plotframe(fa(:,sel)) ;
+set(h1,'color','y','linewidth',3) ;
+fb(1,:) = fb(1,:) + size(Ia,2) ;
+h2 = vl_plotframe(fb(:,sel)) ;
+set(h2,'color','y','linewidth',3) ;
+
+xa = fa(1,sel) ;
+xb = fb(1,sel) ;
+ya = fa(2,sel) ;
+yb = fb(2,sel) ;
+hold on ;
+h = line([xa ; xb], [ya ; yb]) ;
+set(h,'linewidth', 1, 'color', 'r') ;
