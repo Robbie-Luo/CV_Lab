f84fe4033dc183c6d34b483b63ff92865071f8a6
Robbie <luobutang@gmail.com>
2019-03-07 20:36:57 +0100

    Add image transform with imwarp

--- Lab4/RANSAC.m
+++ Lab4/RANSAC.m
@@ -1,36 +1,37 @@
 % Create a function that performs the RANSAC algorithm as explained above. The function should return the best  % transformation found. For visualization, show the transformations from image1 to image2 and from
 % image2 to image1. Name your script as RANSAC.m.
 
-function [best_trans]=RANSAC(fa,fb)
+function [best_trans,best_count]=RANSAC(fa,fb)
 best_count=0;
 N=50;P=10;
 for n = 1:N
     perm=randperm(size(fa,2));
-    for i = perm(1:P)
-        xa=fa(1,i);ya=fa(2,i);
-        xb=fb(1,i);yb=fb(2,i);
-        % calculate the parameters
-        trans=get_trans_params(xa,ya,xb,yb);
-        % transform using the parameters
-        count=0;
-        for j=1:size(fa,2)
-            [x_t,y_t]=transform(fa(1,j),fa(2,j),trans);
-            if(norm([x_t-fb(1,j) y_t-fb(2,j)])<=10)
-                count=count+1;
-            end
+    xa=fa(1,perm(1:P));ya=fa(2,perm(1:P));
+    xb=fb(1,perm(1:P));yb=fb(2,perm(1:P));
+    % calculate the parameters
+    trans=get_trans_params(xa,ya,xb,yb);
+    % transform using the parameters
+    count=0;
+    for j=1:size(fa,2)
+        [x_t,y_t]=transform(fa(1,j),fa(2,j),trans);
+        if(norm([x_t-fb(1,j) y_t-fb(2,j)])<=10)
+            count=count+1;
         end
-        if count>best_count
-            best_count=count;
-            best_trans=trans;
-        end
+    end
+    if count>best_count
+        best_count=count;
+        best_trans=trans;
     end
 end
 end
 
 function [trans] = get_trans_params(xa,ya,xb,yb)
-    A=[xa ya 0 0 1 0;
-       0 0 xa ya 0 1];
-    b=[xb;yb];
+    A=zeros(2*size(xa,2),6);b=zeros(2*size(xa,2),1);
+    for i=1:size(xa,2)
+       A=[A;xa(i) ya(i) 0 0 1 0;
+            0 0 xa(i) ya(i) 0 1];
+       b=[b;xb(i);yb(i)];
+    end
     trans = pinv(A)*b;
 end
 
--- Lab4/main.m
+++ Lab4/main.m
@@ -2,7 +2,7 @@ clc;clear all;close all;
 Ia=im2double(imread("boat1.pgm"));
 Ib=im2double(imread("boat2.pgm"));
 [fa,fb]=keypoint_matching(Ia,Ib);
-[best_trans]=RANSAC(fa,fb)
+[best_trans,best_count]=RANSAC(fa,fb)
 % transform using imwarp
 T=[best_trans(1) best_trans(2) 0;
    best_trans(3) best_trans(4) 0;
@@ -11,5 +11,5 @@ tform = affine2d(T);
 subplot(1,2,1)
 imshow(Ia)
 subplot(1,2,2)
-Ia_t=imwarp(Ia,tform,'nearest');
+Ia_t=imwarp(Ib,tform,'nearest');
 imshow(Ia_t)
